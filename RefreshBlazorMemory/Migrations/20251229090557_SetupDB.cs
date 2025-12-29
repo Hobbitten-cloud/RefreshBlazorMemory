da1ec7086b1cddab2aed714b79319ca65a9e0ab4
Hobbitten <janenhansen@gmail.com>
2025-12-29 10:19:23 +0100

    Made some learning about editforms

---
+++ RefreshBlazorMemory/Migrations/20251229090557_SetupDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefreshBlazorMemory.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetupDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
