abedec0f8e29060b0564fe2a2cf74726ba98e89d
Hobbitten <janenhansen@gmail.com>
2025-12-29 10:33:12 +0100

    added validation

--- RefreshBlazorMemory/Models/Music.cs
+++ RefreshBlazorMemory/Models/Music.cs
@@ -1,9 +1,12 @@
-﻿namespace RefreshBlazorMemory.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RefreshBlazorMemory.Models
 {
     public class Music
     {
         public int Id { get; set; }
-        public string? Artist { get; set; }
+        [Required]
+        public string Artist { get; set; }
         public string? AlbumTitle { get; set; }
         public int ReleaseYear { get; set; }
         public string? Description { get; set; }
